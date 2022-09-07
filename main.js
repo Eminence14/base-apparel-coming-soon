c8b8d3172cd8026212dbb79882e5c19a7820cfeb
Victor <victoribidunmoye@gmail.com>
2022-09-07 12:03:44 +0100

    error message added, no functionality yet

---
+++ main.js
@@ -0,0 +1,11 @@
+const enter = document.querySelector('#enter')
+let input = document.querySelector('input[type="text"]')
+
+enter.addEventListener('click', () => {
+    input.style.borderColor = 'hsl(0, 93%, 68%)'
+    if (input.value) {
+        
+    } else {
+        
+    }
+})
